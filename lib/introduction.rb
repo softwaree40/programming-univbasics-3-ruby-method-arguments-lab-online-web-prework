f9c2d9e7d15134aa3725d2e1245ef469c71f2ff3
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-17 02:43:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,12 @@
 def introduction(name)
-<<<<<<< HEAD
+
    puts "Hi, my name is #{name}."
   
 end
-=======
+
   return   'my name is' + name
   
   
 end
 puts introduction('josh')
->>>>>>> 4d12328de75c672017219650d3b86146b0c5da29
+
